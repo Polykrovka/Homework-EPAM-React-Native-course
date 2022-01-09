a7c86a1d728a55410a86157ade458696dc2a8c86
anton-nedved <Anton_Nedved@epam.com>
2022-01-09 18:01:59 +0300

    error resolution

--- App.js
+++ App.js
@@ -3,8 +3,10 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {MainScreen} from './src/pages/Main-screen';
 import {Product} from './src/pages/Product';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const App = () => {
   return (
@@ -17,6 +19,10 @@ const App = () => {
         <Stack.Screen name="MainScreen" component={MainScreen} />
         <Stack.Screen name="Product" component={Product} />
       </Stack.Navigator>
+      {/* <Drawer.Navigator>
+        <Drawer.Screen name="MainScreen" component={MainScreen} />
+        <Drawer.Screen name="Product" component={Product} />
+      </Drawer.Navigator> */}
     </NavigationContainer>
   );
 };
