fea3f95669ca95bfef79714821743e62a4bc222d
anton-nedved <Anton_Nedved@epam.com>
2022-01-09 23:46:01 +0300

    add Unregistered cart

---
+++ src/components/Unregistered-cart.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {StyleSheet, View, Text, Pressable} from 'react-native';
+import {commonStyles} from '../styles/common-styles';
+import {useNavigation} from '@react-navigation/native';
+import IconAvatar from '../images/icons/Icon-Avatar.svg';
+
+export const UnregisteredCart = () => {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <IconAvatar style={styles.icon} />
+      <Text style={styles.title}>Login First!</Text>
+      <Text style={styles.discription}>Login first to view your cart</Text>
+      <Pressable style={styles.button} onPress={() => navigation.goBack()}>
+        <Text style={styles.buttonText}>LOGIN NOW</Text>
+      </Pressable>
+      <Text style={styles.signUpLabel}>New here? Sign Up</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'white',
+    alignSelf: 'center',
+    alignItems: 'center',
+  },
+  icon: {
+    marginTop: 130,
+  },
+  title: {
+    fontSize: 20,
+    color: '#8F8F8F',
+    fontWeight: 'bold',
+    marginTop: 14.5,
+  },
+  button: {
+    backgroundColor: '#008ACE',
+    width: '90%',
+    height: 40,
+    borderRadius: 4,
+    ...commonStyles.shadow,
+    bottom: 10,
+    marginTop: 37,
+  },
+  buttonText: {
+    fontSize: 15,
+    textAlign: 'center',
+    marginTop: 10,
+    color: 'white',
+  },
+  discription: {
+    width: '55%',
+    fontSize: 15,
+    textAlign: 'center',
+    marginTop: 10,
+  },
+  signUpLabel: {
+    color: '#00A8F3',
+    fontSize: 15,
+  },
+});
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {StyleSheet, Text} from 'react-native';
 import {NavBarCart} from '../../src/components/Nav-bar-cart';
 import {RegistredCart} from '../components/Registered-cart';
+import {UnregisteredCart} from '../components/Unregistered-cart';
 
 export const Cart = () => {
+  let registered = false;
   return (
     <>
       <NavBarCart />
-      <RegistredCart />
+      {registered ? <RegistredCart /> : <UnregisteredCart />}
     </>
   );
 };
